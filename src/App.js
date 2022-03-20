17c066282676c1f8ad129bae26b13fba90ca6931
noordseoostende <voropayev1@mail.ru>
2022-03-20 19:02:53 +0100

    een album toevoegen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Destinations from './components/Destinations';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
 
 
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,15 @@ import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import { FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube } from "react-icons/fa";
 const Navbar = () => {
   const [nav, setNav] = useState(false)
+  const [logo, setLogo] = useState(false)
   const handleNav = () => {
     setNav(!nav)
+    setLogo(!logo)
   }
   return (
-    <div className='flex justify-between items-center h-20 px-4'>
-      <div>
-        <h1>TUI</h1>
+    <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
+      <div >
+        <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>TUI</h1>
       </div>
       <ul className="hidden md:flex">
         <li>Home</li>
@@ -22,14 +24,14 @@ const Navbar = () => {
         <li>Book</li>
       </ul>
       <div className="hidden md:flex">
-        <BiSearch size={20}/>
+        <BiSearch className='mr-2' size={20}/>
         <BsPerson size={20}/>
       </div>
-      <div onClick={handleNav} className='md:hidden '>
-        {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20}/>}
+      <div onClick={handleNav} className='md:hidden z-10'>
+        {nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={20}/>}
         
       </div>
-      <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex-col' : 'absolute left-[-100%]'}>
+      <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex-col' : 'absolute left-[-100%]'}>
         <ul>
           <h1>TUI</h1>
         <li className='border-b'>Home</li>
